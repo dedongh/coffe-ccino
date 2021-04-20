d7c265e45643e6c60926eb13bed3d29e37b76596
braemma <asiamahemma95@gmail.com>
2021-04-20 04:49:01 +0000

    Initial Commit

---
+++ CoffeeDB/CoffeeDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeDBApp.swift
+//  CoffeeDB
+//
+//  Created by Bra Emma on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
